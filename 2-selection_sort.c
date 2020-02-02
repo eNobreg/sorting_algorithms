8cd7a378d97eaf11936b30d0cfa495de4092ad38
eNobreg <ezra.nobrega@outlook.com>
2020-02-02 17:28:09 -0500

    [Change] Added efficiency to not print extra

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,7 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	size_t j = 0;
-	size_t min = 0; 
+	size_t min = 0;
 	int temp = 0;
 
 	if (size == 1)
@@ -25,9 +25,12 @@ void selection_sort(int *array, size_t size)
 				if (array[j] < array[min])
 					min = j;
 			}
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
+			if (min != i)
+			{
+				temp = array[i];
+				array[i] = array[min];
+				array[min] = temp;
+				print_array(array, size);
+			}
 	}
 }
