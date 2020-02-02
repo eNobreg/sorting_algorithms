a31065c0148c8fe4d19a92d2d91390f65cc53c63
eNobreg <ezra.nobrega@outlook.com>
2020-02-02 15:05:22 -0500

    [New] Iitial push

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Implementaiton of the shell sort algorithm
+ * @array: Unsorted array
+ * @size: Size of the array
+ * Return: Never
+ */
+void shell_sort(int *array, size_t size)
+{
+	int temp = 0, flag = 0;
+	size_t i = 0, j = 0;
+	size_t gap = 0;
+
+	for (gap = 1; gap < size; gap = gap * 3 + 1) /* Def gap based on arr size */
+		;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++) /* Move through remaining array after the gap */
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap]; /* Swap val before gap with after the gap */
+				flag = 1;
+			}
+			array[j] = temp;
+		}
+		gap /= 3; /* Divide the gap by three and start again */
+		if (flag == 1) /* If there was a swap */
+			print_array(array, size);
+	}
+}
