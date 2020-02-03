72ff8e72f48f9393e98bc894a5de77a033ca6f23
eNobreg <ezra.nobrega@outlook.com>
2020-02-03 12:57:58 -0500

    [Change] Filtered out null and < 2

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,7 +12,7 @@ void shell_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	size_t gap = 0;
 
-	if (size == 0)
+	if (size < 2 || array == NULL)
 		return;
 
 	for (gap = 1; gap < size; gap = gap * 3 + 1) /* Def gap based on arr size */
