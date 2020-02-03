c853e7e6ba7005576fc5b193b3b3294870027bbe
Michelle Giraldo <michellegsld@gmail.com>
2020-02-03 10:51:25 -0500

    [fix] typo line 13, now checking the absence of list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = NULL, *end = NULL, *move = NULL;
 
-	if (list || ((*list == NULL) || ((*list)->next == NULL)))
+	if (!list || ((*list == NULL) || ((*list)->next == NULL)))
 		return;
 
 	current = (*list)->next;
