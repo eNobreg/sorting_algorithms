6d2ec7e130aef31a5bd1ec8c26dc56b358d29ab5
eNobreg <ezra.nobrega@outlook.com>
2020-02-04 13:13:55 -0500

    [New and Change] New: Initial push for the hoare partition scheme function Change: Updated the header to include 107

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,85 @@
+#include "sort.h"
+int partition(int *array, int start, int end, int size);
+void quicksort(int *array, int start, int end, int size);
+void swap(int *array, int *a, int *b, int size);
+
+/**
+ * quick_sort_hoare - Implementation of quick sort
+ * @array: Array to sort
+ * @size: Size of array
+ * Return: Never
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (size >= 2)
+		quicksort(array, 0, size - 1, size);
+}
+
+/**
+ * quicksort - recursive function for quicksort
+ * @array: Array to start
+ * @start: Start of the array segment
+ * @end: End of the array segment
+ * @size: Size of the whole array
+ * Return: None
+ */
+void quicksort(int *array, int start, int end, int size)
+{
+
+	size_t j = 0;
+
+	if (start < end)
+	{
+		j = partition(array, start, end, size);
+		quicksort(array, start, j - 1, size);
+		quicksort(array, j, end, size);
+
+	}
+}
+
+/**
+ * swap - recursive function for quicksort
+ * @array: Array to start
+ * @a: Index to swap
+ * @b: Index to swap
+ * @size: Size of the whole array
+ * Return: None
+ */
+void swap(int *array, int *a, int *b, int size)
+{
+	int temp = 0;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	if (*a != *b)
+		print_array(array, size);
+}
+
+/**
+ * partition - recursive function for quicksort
+ * @array: Array to start
+ * @start: Start of the array segment
+ * @end: End of the array segment
+ * @size: Size of the whole array
+ * Return: None
+ */
+int partition(int *array, int start, int end, int size)
+{
+	int pivot = array[end], i = start - 1;
+	int j = end + 1;
+
+	while (1)
+	{
+		do {
+			i += 1;
+		} while (array[i] < pivot);
+		do {
+			j -= 1;
+		} while (array[j] > pivot);
+		if (i >= j)
+			return (i);
+		swap(array, &array[i], &array[j], size);
+	}
+	return (i);
+}
