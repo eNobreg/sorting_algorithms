5581044a4c69896c81db968194dc018983041c61
eNobreg <ezra.nobrega@outlook.com>
2020-02-04 17:34:41 -0500

    [Change and New] New: New merge sort file Change: Added merge sort to header, and added error checking to 107

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -11,7 +11,7 @@ void swap(int *array, int *a, int *b, int size);
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (size >= 2)
+	if (array != NULL || size >= 2)
 		quicksort(array, 0, size - 1, size);
 }
 
